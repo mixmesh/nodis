c9f7647de286a53c46707482ef7e693fc8b1c853
Tony Rogvall <tony@rogvall.se>
2020-10-22 19:26:31 +0200

    config schema for nodis

---
+++ src/nodis_schema.erl
@@ -0,0 +1,21 @@
+%%% @author Tony Rogvall <tony@rogvall.se>
+%%% @copyright (C) 2020, Tony Rogvall
+%%% @doc
+%%%    Nodis general config
+%%% @end
+%%% Created : 22 Oct 2020 by Tony Rogvall <tony@rogvall.se>
+
+-module(nodis_schema).
+
+-export([get/0]).
+
+-include_lib("apptools/include/config_schema.hrl").
+-include_lib("apptools/include/shorthand.hrl").
+
+get() ->
+    [{'nodis',
+      {'ping-interval',
+       #json_type{name = {integer,50,1000000},
+		  info = "Interval in ms between multicast pings",
+		  typical = 5000 }}
+     }].
